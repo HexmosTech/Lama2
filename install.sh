69f66e21fc9b0aecda84516bd3b603023d09cf83
varunvd <varun.vd51@gmail.com>
2021-12-29 19:26:40 +0530

    Installation script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cmd=http
+[[ $(type -P "$cmd") ]] || 
+    { echo "$cmd is NOT in PATH" 1>&2; sudo snap install http; }
+go build
+mv httpelf build
+echo "Find httpelf binary in ./build/ directory"
+sudo cp ./build/httpelf /usr/bin
