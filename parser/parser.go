e60ce8319c1dadcbf43109c94df818969f7d8c41
Shrijith Venkatramana <shrijith@hexmos.com>
2022-10-28 18:17:19 +0530

    Move char handling to separate file

--- parser/parser.go
+++ parser/parser.go
@@ -1,7 +1,6 @@
 package parser
 
 import (
-	"errors"
 	"fmt"
 	"os"
 
@@ -48,82 +47,6 @@ func (p *Parser) SetText(text string) {
 	p.pos = -1
 	p.tlen = len(p.text) - 1
 	p.cache = make(map[string][]string)
-}
-
-func (p *Parser) Char() (rune, error) {
-	if p.pos >= p.tlen {
-		return rune(0),
-			utils.NewParseError(
-				p.pos,
-				"Expected %s but got end of string",
-				[]string{"character"})
-	}
-	next_char := p.text[p.pos+1]
-	p.pos += 1
-	return next_char, nil
-}
-
-func (p *Parser) CharClass(charClass string) (rune, error) {
-	if p.pos >= p.tlen {
-		return rune(0),
-			utils.NewParseError(
-				p.pos,
-				"Expected %s but got end of string",
-				[]string{"character"})
-	}
-	nextChar := p.text[p.pos+1]
-	charRangeList, e := p.SplitCharRanges(charClass)
-	if e != nil {
-		fmt.Errorf("%s", e)
-		os.Exit(1)
-	}
-
-	for _, charRange := range charRangeList {
-		runeCharRange := []rune(charRange)
-		if len(runeCharRange) == 1 {
-			if nextChar == runeCharRange[0] {
-				p.pos += 1
-				return nextChar, nil
-			}
-		} else if runeCharRange[0] <= nextChar && nextChar <= runeCharRange[2] {
-			p.pos += 1
-			return nextChar, nil
-		}
-	}
-
-	return rune(0),
-		utils.NewParseError(
-			p.pos,
-			"Expected from character class but no match",
-			[]string{})
-}
-
-func (p *Parser) SplitCharRanges(charClass string) ([]string, error) {
-	val, prs := p.cache[charClass]
-	if prs {
-		return val, nil
-	}
-
-	runeCharClass := []rune(charClass)
-	rv := make([]string, 0)
-	index := 0
-	length := len(runeCharClass)
-	for index < length {
-		if index+2 < length && runeCharClass[index+1] == '-' {
-			if runeCharClass[index] >= runeCharClass[index+2] {
-				return []string{""}, errors.New("bad character range")
-			}
-
-			rv = append(rv, string(runeCharClass[index:index+3]))
-			index += 3
-		} else {
-			rv = append(rv, string(runeCharClass[index]))
-			index += 1
-		}
-	}
-
-	p.cache[charClass] = rv
-	return rv, nil
 }
 
 func (p *Parser) assertEnd() (bool, error) {
--- tests/parser_test.go
+++ tests/parser_test.go
@@ -8,16 +8,6 @@ import (
 	"github.com/hexmos/lama2/parser"
 )
 
-func TestParser(t *testing.T) {
-	p := parser.Parser{}
-	got := p.Parse("GET http://google.com")
-	want := "blah"
-
-	if got != want {
-		t.Errorf("got %q, wanted %q", got, want)
-	}
-}
-
 func TestLama2Parser(t *testing.T) {
 	p := parser.NewLama2Parser()
 	got := p.Parse("GET http://google.com")
