87dc98a397662eefb5a406d0ec0c6b42b2102698
Shrijith Venkatramana <shrijith@hexmos.com>
2022-11-13 13:42:13 +0530

    Generalize os string, fix minor bugs

--- install.sh
+++ install.sh
@@ -6,44 +6,57 @@ get_file() {
 }
 
 get_platform() {
-   architecture=""
-   case $(uname -m) in
-       i386)   architecture="386" ;;
-       i686)   architecture="386" ;;
-       x86_64) architecture="amd64" ;;
-       arm)    dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
-   esac 
+    architecture=""
+    case $(uname -m) in
+    i386) architecture="386" ;;
+    i686) architecture="386" ;;
+    x86_64) architecture="amd64" ;;
+    arm) dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
+    esac
 }
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "OS is Linux"
-    get_file 
-    get_platform
-    # echo "$api_resp" | grep linux
-    search="http.*linux-$architecture.tar.gz\"$"
-    echo $search
-    archive=$(echo "$api_resp" | grep "$search" | sed 's|[\"\,]*||g' | sed 's/browser_download_url://g' | xargs)
-    wget -O /tmp/l2_latest.tar.gz $archive
-    tar -xvzf /tmp/l2_latest.tar.gz -C /tmp
-    sudo rm -f /usr/local/bin/l2 /usr/bin/l2
-    sudo mv /tmp/l2 /usr/local/bin
-elif [[ "$OSTYPE" == "darwin"* ]]; then
+get_os() {
+    the_os=""
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        echo "OS is Linux"
+        the_os="linux"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
         # Mac OSX
-    echo "OS is Mac OSX"
-elif [[ "$OSTYPE" == "cygwin" ]]; then
+        echo "OS is Mac OSX"
+        the_os="darwin" 
+    elif [[ "$OSTYPE" == "cygwin" ]]; then
         # POSIX compatibility layer and Linux environment emulation for Windows
-    echo "OS is Cygwin"
-elif [[ "$OSTYPE" == "msys" ]]; then
+        echo "OS is Cygwin"
+        echo "Installer not supported yet; please use release binary"
+        exit
+    elif [[ "$OSTYPE" == "msys" ]]; then
         # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-    echo "OS is msys"
-elif [[ "$OSTYPE" == "win32" ]]; then
+        echo "OS is msys"
+        echo "Installer not supported yet; please use release binary"
+        exit
+    elif [[ "$OSTYPE" == "win32" ]]; then
         # I'm not sure this can happen.
-    echo "OS is win32"
-elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        echo "OS is win32"
+        echo "Installer not supported yet; please use release binary"
+        exit
+    elif [[ "$OSTYPE" == "freebsd"* ]]; then
         # ...
-    echo "OS is freebsd"
-else
+        echo "OS is freebsd"
+        echo "Installer not supported yet; please use release binary"
+        exit
+    else
         # Unknown.
-    echo "Error: Unknown OS"
-fi
+        echo "Error: Unknown OS"
+    fi
+}
 
+get_file
+get_platform
+get_os
+search="http.*$the_os-$architecture.tar.gz\"$"
+echo $search
+archive=$(echo "$api_resp" | grep "$search" | sed 's|[\"\,]*||g' | sed 's/browser_download_url://g' | xargs)
+wget -O /tmp/l2_latest.tar.gz $archive
+tar --overwrite -xvzf /tmp/l2_latest.tar.gz -C /tmp
+sudo rm -f /usr/local/bin/l2 /usr/bin/l2
+sudo mv /tmp/l2 /usr/local/bin
