71829f85d186bea72865ed61faaa8bf4511a2973
Shrijith Venkatramana <shrijith@hexmos.com>
2022-11-13 15:55:10 +0530

    Ensure http and other installation improvements

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ get_os() {
     elif [[ "$OSTYPE" == "darwin"* ]]; then
         # Mac OSX
         echo "OS is Mac OSX"
-        the_os="darwin" 
+        the_os="darwin"
     elif [[ "$OSTYPE" == "cygwin" ]]; then
         # POSIX compatibility layer and Linux environment emulation for Windows
         echo "OS is Cygwin"
@@ -51,13 +51,47 @@ get_os() {
     fi
 }
 
+ensure_http() {
+    if python3 --version; then 
+        python3 -m pip install httpie
+        return
+    fi
+
+    if python --version; then
+        python -m pip install httpie
+        return
+    fi
+    
+    # We are using the universal, python-pip based
+    # installation method right now. However, if needed
+    # we can switch to native installation machinery
+    #
+    #     if [[ ${the_os} == "linux" ]]; then
+    #         cmd=http
+    #         [[ -n $(type -P "${cmd}") ]] ||
+    #             {
+    #                 echo "${cmd} is NOT in PATH" 1>&2
+    #                 sudo snap install http
+    #             }
+    #     elif [[ ${the_os} == "darwin" ]]; then
+    #         cmd=http
+    #         [[ -n $(type -P "${cmd}") ]] ||
+    #             {
+    #                 echo "${cmd} is NOT in PATH" 1>&2
+    #                 brew install httpie
+    #             }
+    # 
+    #     fi
+}
+
 get_file
 get_platform
 get_os
-search="http.*$the_os-$architecture.tar.gz\"$"
-echo "$search"
-archive=$(echo "$api_resp" | grep "$search" | sed 's|[\"\,]*||g' | sed 's/browser_download_url://g' | xargs)
-wget -O /tmp/l2_latest.tar.gz "$archive"
+ensure_http
+search="http.*${the_os}-${architecture}.tar.gz\"$"
+echo "${search}"
+archive=$(echo "${api_resp}" | grep "${search}" | sed 's|[\"\,]*||g' | sed 's/browser_download_url://g' | xargs)
+wget -O /tmp/l2_latest.tar.gz "${archive}"
 tar --overwrite -xvzf /tmp/l2_latest.tar.gz -C /tmp
 sudo rm -f /usr/local/bin/l2 /usr/bin/l2
 sudo mv /tmp/l2 /usr/local/bin
