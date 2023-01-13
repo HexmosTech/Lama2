54c189f5253dd2a8dc1c2af9094e1779fb2b59e1
Shrijith Venkatramana <shrijith@hexmos.com>
2023-01-13 11:33:38 +0530

    Print Exception if any in the code

--- cmdexec/js.go
+++ cmdexec/js.go
@@ -4,11 +4,27 @@ import (
 	"fmt"
 
 	"github.com/dop251/goja"
+	"github.com/dop251/goja_nodejs/console"
+	"github.com/dop251/goja_nodejs/require"
 )
 
 func TestJS() {
 	vm := goja.New()
-	_, err := vm.RunString("let url = 'https://google.com'; let m = 100; obj = JSON.stringify({'a': 1, 'b': 2});")
+	new(require.Registry).Enable(vm)
+	console.Enable(vm)
+	_, err := vm.RunString(`
+	js = JSON.stringify
+	let url = 'https://google.com'; 
+	let m = 100; 
+	obj = js({'a': 1, 'b': 2});
+	o2 = 0
+	for (let i = 1; i <= 10; i++) {
+		o2 += i
+	}
+	`)
+	if ex, ok := err.(*goja.Exception); ok {
+		fmt.Println(ex.String())
+	}
 	// url := vm.Get("url")
 	// fmt.Println(url.String())
 
@@ -21,6 +37,9 @@ func TestJS() {
 	vm.RunString("let m2 = m*5")
 	g2 := vm.Get("m2")
 	fmt.Println(g2.String())
+
+	g3 := vm.Get("o2")
+	fmt.Println(g3.String())
 
 	if err != nil {
 		panic(err)
--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.17
 require (
 	github.com/HexmosTech/gabs/v2 v2.6.3
 	github.com/creack/pty v1.1.18
+	github.com/dop251/goja v0.0.0-20230111111035-473251c96b4c
+	github.com/dop251/goja_nodejs v0.0.0-20221211191749-434192f0843e
 	github.com/jessevdk/go-flags v1.5.0
 	github.com/joho/godotenv v1.4.0
 	github.com/rs/zerolog v1.28.0
@@ -12,10 +14,9 @@ require (
 
 require (
 	github.com/dlclark/regexp2 v1.7.0 // indirect
-	github.com/dop251/goja v0.0.0-20230111111035-473251c96b4c // indirect
 	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
-	golang.org/x/sys v0.2.0 // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
 )
