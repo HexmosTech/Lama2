021df9b577d2d716aacaa1d084e00906e92aa3d4
Shrijith Venkatramana <shrijith@hexmos.com>
2023-01-23 21:12:22 +0530

    JS VM test, cleanups

--- tests/js_test.go
+++ tests/js_test.go
@@ -1,11 +1,39 @@
 package tests
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/HexmosTech/lama2/cmdexec"
 )
 
-func TestJS(t *testing.T) {
-	cmdexec.TestJS()
+func TestRunVMCode(t *testing.T) {
+	vm := cmdexec.GetJSVm()
+	cmdexec.RunVMCode("let meaning=42", vm)
+}
+
+func TestJSVmNegative(t *testing.T) {
+	vm := cmdexec.GetJSVm()
+	_, e := vm.RunString("faulty JS code")
+	if e == nil {
+		t.Errorf("Expected failure on faulty JS code")
+	}
+}
+
+func TestJSVm(t *testing.T) {
+	vm := cmdexec.GetJSVm()
+	_, e := vm.RunString("let x=10")
+	if e != nil {
+		t.Errorf("Expected nil on valid JS code")
+	}
+}
+
+func TestGenerateChainCode(t *testing.T) {
+	generatedStr := cmdexec.GenerateChainCode(`{"hello": "world"}`)
+	if strings.HasPrefix(generatedStr, "try") == true {
+		fmt.Println("Try/Catch in TestGenerateChain")
+	} else {
+		t.Fatalf("TestGenerateChain doesn't start with a try")
+	}
 }
