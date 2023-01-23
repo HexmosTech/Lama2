dd79bdaf8235c4a028367029e8d8f331ba6c2754
Shrijith Venkatramana <shrijith@hexmos.com>
2023-01-23 20:10:47 +0530

    Refactor; move methods to the appropriate files

--- cmdexec/js.go
+++ cmdexec/js.go
@@ -1,47 +1,35 @@
 package cmdexec
 
 import (
-	"fmt"
-
 	"github.com/dop251/goja"
 	"github.com/dop251/goja_nodejs/console"
 	"github.com/dop251/goja_nodejs/require"
+	"github.com/rs/zerolog/log"
 )
 
-func TestJS() {
+func GetJSVm() *goja.Runtime {
 	vm := goja.New()
 	new(require.Registry).Enable(vm)
 	console.Enable(vm)
-	_, err := vm.RunString(`
-	js = JSON.stringify
-	let url = 'https://google.com'; 
-	let m = 100; 
-	obj = js({'a': 1, 'b': 2});
-	o2 = 0
-	for (let i = 1; i <= 10; i++) {
-		o2 += i
-	}
-	`)
+	return vm
+}
+
+func RunVMCode(chainCode string, vm *goja.Runtime) {
+	_, err := vm.RunString(chainCode)
 	if ex, ok := err.(*goja.Exception); ok {
-		fmt.Println(ex.String())
+		log.Fatal().Str("Error executing JS processor block", ex.String()).Msg("")
 	}
-	// url := vm.Get("url")
-	// fmt.Println(url.String())
+}
 
-	var s string
-	g := vm.Get("obj")
-	fmt.Println(g.String())
-	vm.ExportTo(g, s)
-	fmt.Println(s)
-
-	vm.RunString("let m2 = m*5")
-	g2 := vm.Get("m2")
-	fmt.Println(g2.String())
-
-	g3 := vm.Get("o2")
-	fmt.Println(g3.String())
-
-	if err != nil {
-		panic(err)
-	}
+func GenerateChainCode(resp string) string {
+	code := `try {
+		result = JSON.parse(String.raw` + "`" + resp + "`" + `)	
+		console.log("Stored as JSON")
+	} catch (e) {
+		result = String.raw` + "`" + resp + "`" + ` 	
+		console.log(e)
+		console.log("Stored as string")
+	}`
+	log.Debug().Str("Chain code generated", code).Msg("")
+	return code
 }
--- controller/controller.go
+++ controller/controller.go
@@ -13,119 +13,36 @@ import (
 	"github.com/HexmosTech/httpie-go"
 	"github.com/HexmosTech/lama2/cmdexec"
 	"github.com/HexmosTech/lama2/cmdgen"
-	"github.com/HexmosTech/lama2/importer"
 	"github.com/HexmosTech/lama2/lama2cmd"
 	outputmanager "github.com/HexmosTech/lama2/outputManager"
 	"github.com/HexmosTech/lama2/parser"
 	"github.com/HexmosTech/lama2/preprocess"
 	"github.com/HexmosTech/lama2/utils"
 	"github.com/dop251/goja"
-	"github.com/dop251/goja_nodejs/console"
-	"github.com/dop251/goja_nodejs/require"
 	"github.com/rs/zerolog/log"
 )
 
-func expandHeaders(block *gabs.Container, vm *goja.Runtime) {
-	headerMap := block.S("details", "headers")
-	log.Debug().Str("HeaderMap", headerMap.String()).Msg("")
-	if headerMap == nil {
-		return
-	}
-	newHeaderMap := gabs.New()
-	for k, v := range headerMap.ChildrenMap() {
-		log.Trace().Strs("Header pair", []string{k, " = ", v.String()}).Msg("")
-		key := preprocess.ExpandEnv(k, vm)
-		val := preprocess.ExpandEnv(v.Data().(*gabs.Container).Data().(string), vm)
-		valWrap := gabs.New()
-		valWrap.Set(val)
-		newHeaderMap.Set(valWrap, key)
-	}
-	block.Delete("details", "headers")
-	block.Set(newHeaderMap, "details", "headers")
-	log.Debug().Str("Expanded Header block", block.String()).Msg("")
-}
-
-func expandURL(block *gabs.Container, vm *goja.Runtime) {
-	b := block.S("url", "value").Data().(string)
-	log.Debug().Str("Url block", b).Msg("")
-	url := preprocess.ExpandEnv(b, vm)
-	block.Delete("url", "value")
-	block.Set(url, "url", "value")
-}
-
-func expandJSON(block *gabs.Container, vm *goja.Runtime) {
-	log.Debug().Str("JSON block to be expanded", block.String()).Msg("")
-	dataBlock := block.S("details", "ip_data")
-	if dataBlock == nil {
-		return
-	}
-	dataBlockStr := dataBlock.String()
-	dataBlockStr = preprocess.ExpandEnv(dataBlockStr, vm)
-	log.Debug().Str("Expanded JSON data block", dataBlockStr).Msg("")
-	processedBlock, err := gabs.ParseJSON([]byte(dataBlockStr))
-	if err != nil {
-		log.Error().Str("Preprocess JSON block issue", "").Msg("")
-		return
-	}
-	block.Delete("details", "ip_data")
-	block.Set(processedBlock, "details", "ip_data")
-	log.Debug().Str("Processed JSON block", block.String()).Msg("")
-}
-
-func runVMCode(chainCode string, vm *goja.Runtime) {
-	_, err := vm.RunString(chainCode)
-	if ex, ok := err.(*goja.Exception); ok {
-		log.Fatal().Str("Error executing JS processor block", ex.String()).Msg("")
-	}
-}
-
-func generateChainCode(resp string) string {
-	code := `try {
-		result = JSON.parse(String.raw` + "`" + resp + "`" + `)	
-		console.log("Stored as JSON")
-	} catch (e) {
-		result = String.raw` + "`" + resp + "`" + ` 	
-		console.log(e)
-		console.log("Stored as string")
-	}`
-	log.Debug().Str("Chain code generated", code).Msg("")
-	return code
-}
-
 func GetParsedAPIBlocks(parsedAPI *gabs.Container) []*gabs.Container {
 	return parsedAPI.S("value").Data().(*gabs.Container).Children()
-}
-
-func GetJSVm() *goja.Runtime {
-	vm := goja.New()
-	new(require.Registry).Enable(vm)
-	console.Enable(vm)
-	return vm
 }
 
 func ExecuteProcessorBlock(block *gabs.Container, vm *goja.Runtime) {
 	b := block.S("value").Data().(*gabs.Container)
 	log.Debug().Str("Processor block incoming block", block.String()).Msg("")
 	script := b.Data().(string)
-	runVMCode(script, vm)
-}
-
-func ProcessVarsInBlock(block *gabs.Container, vm *goja.Runtime) {
-	expandURL(block, vm)
-	expandHeaders(block, vm)
-	expandJSON(block, vm)
+	cmdexec.RunVMCode(script, vm)
 }
 
 func ExecuteRequestorBlock(block *gabs.Container, vm *goja.Runtime, opts *lama2cmd.Opts, dir string) httpie.ExResponse {
-	ProcessVarsInBlock(block, vm)
+	preprocess.ProcessVarsInBlock(block, vm)
 	// TODO - replace stuff in headers, and varjson and json as well
 	cmd, stdinBody := cmdgen.ConstructCommand(block, opts)
 	log.Debug().Str("Stdin Body to be passed into httpie", stdinBody).Msg("")
 	resp, e1 := cmdexec.ExecCommand(cmd, stdinBody, dir)
 	log.Debug().Str("Response from ExecCommand", resp.Body).Msg("")
 	if e1 == nil {
-		chainCode := generateChainCode(resp.Body)
-		runVMCode(chainCode, vm)
+		chainCode := cmdexec.GenerateChainCode(resp.Body)
+		cmdexec.RunVMCode(chainCode, vm)
 	} else {
 		log.Fatal().Str("Error from ExecCommand", e1.Error())
 		os.Exit(1)
@@ -133,35 +50,9 @@ func ExecuteRequestorBlock(block *gabs.Container, vm *goja.Runtime, opts *lama2c
 	return resp
 }
 
-func ArgParsing(o lama2cmd.Opts, version string) {
-	if o.Version {
-		fmt.Println(version)
-		return
-	}
-	if o.Update {
-		utils.UpdateSelf()
-		return
-	}
-	if len(o.PostmanFile) > 0 {
-		if len(o.LamaDir) > 0 {
-			importer.PostmanImporter(o.PostmanFile, o.LamaDir)
-			os.Exit(1)
-		}
-		log.Fatal().Msg("To convert Postman export to Lama2, try: l2 -p PostmanFile -l Lama2Dir")
-	}
-	if len(o.LamaDir) > 0 {
-		if len(o.PostmanFile) > 0 {
-			importer.PostmanImporter(o.PostmanFile, o.LamaDir)
-			os.Exit(1)
-		}
-		log.Fatal().Msg("To convert Postman export to Lama2, try: l2 -p PostmanFile -l Lama2Dir")
-	}
-
-}
-
 func HandleParsedFile(parsedAPI *gabs.Container, o *lama2cmd.Opts, dir string) {
 	parsedAPIblocks := GetParsedAPIBlocks(parsedAPI)
-	vm := GetJSVm()
+	vm := cmdexec.GetJSVm()
 	var resp httpie.ExResponse
 	for i, block := range parsedAPIblocks {
 		log.Debug().Int("Block num", i).Msg("")
@@ -189,6 +80,7 @@ func HandleParsedFile(parsedAPI *gabs.Container, o *lama2cmd.Opts, dir string) {
 // 7. Optionally, post-process and write results to a JSON file
 func Process(version string) {
 	o := lama2cmd.GetAndValidateCmd(os.Args)
+	lama2cmd.ArgParsing(o, version)
 	apiContent := preprocess.GetLamaFileAsString(o.Positional.LamaAPIFile)
 	_, dir, _ := utils.GetFilePathComponents(o.Positional.LamaAPIFile)
 	preprocess.LoadElfEnv(path.Join(dir, "l2.env"))
