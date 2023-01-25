5636c30c589dedfb5bbdc40771e7efb7f5c32b39
Shrijith Venkatramana <shrijith@hexmos.com>
2023-01-25 21:30:49 +0530

    Renew Iro syntax exports in repo

--- syntax/README.md
+++ syntax/README.md
@@ -17,6 +17,14 @@ Iro generates a Textmate syntax file in the `Plist` format. We
 must convert the `Plist` [file](./textmate.plist) into JSON using [wtools](https://wtools.io/convert-plist-to-json). If the converted output is wrapped up in a list `[]`, then remove that part. After removing
 unnecessary junk, we will have a nice syntax JSON of the form `{}`. See the VSCode plugin, particularly [package.json](https://github.com/HexmosTech/Lama2Code/blob/main/package.json#L38) and [syntaxes/lama2.tmGrammar.json](https://github.com/HexmosTech/Lama2Code/blob/main/syntaxes/lama2.tmGrammar.json)
 
+**UPDATE: ** After adding request chaining, the generated `plist` doesn't work as is (with the above minor mods). It took a difficult 5h debugging session to find many issues with how textmate syntax handles line-ends. The present grammar json file in VSCode is heavily modified from the original. 
+
+Learnings:
+
+1. "Chaining begin/end" so that "end" tokens are properly consumed [ref1](https://github.com/microsoft/vscode-textmate/issues/41)
+1. Exploiting oniguruma regex implementation (particularly lookbehind). Iro doesn't support lookbehind. But VSCode-oniguruma does thankfully. [ref2](https://github.com/microsoft/vscode-oniguruma/blob/d0c4721b1c8bbee8a63bc9b2e0b6b0f9d3a9b698/src/onig.cc)
+1. 
+
 ### Other
 
 - [Atom](./atom)
--- syntax/rouge.rb
+++ syntax/rouge.rb
@@ -9,27 +9,26 @@ module Rouge
       filenames '*.l2', '*.lama2'
 
       state:root do
-          rule /(#.*)/, Comment
-          rule /(GET|POST|PUT|DELETE|get|post|put|delete)/, Keyword
-          rule /(MULTIPART|multipart)/, Keyword
-          rule /(https?|\$\{.*\})(.*)/ do
-            groups Name::Variable, Punctuation
-          end
-          rule /(\{)/, Punctuation, :json__1
-          rule /(\[)/, Punctuation, :json__2
-          rule /(,|\:)/, Punctuation
-          rule /(\")/, Punctuation, :json__3
-          rule /((?:\-?(?:0|(?:[1-9][0-9]*)))(?:(?:\.[0-9]*)(?:(?:e|E)(?:\+|\-)[0-9]+)?)?)/, Keyword::Type
-          rule /(true|false|null)/, Keyword::Type
-          rule /(.*)(:)([^#\n\r]+)/ do
-            groups Name::Constant, Punctuation, Literal
-          end
-          rule /(.*)(=)([^#\n\r]+)/ do
-            groups Name::Constant, Punctuation, Literal
-          end
-          rule /(.*)(@)([^#\n\r]+)/ do
-            groups Name::Constant, Punctuation, Literal
-          end
+          rule /(^\s*$)/, String::Doc
+          rule /(^---$)/, Punctuation
+          rule /(^\s*(GET|POST|PUT|DELETE|get|post|put|delete))/, Keyword, :root_lama
+          rule /(^\s*#.*)/, Comment, :root_lama
+          rule /(?!(^\s*(#|GET|POST|PUT|DELETE|get|post|put|delete)))/, Name::Variable, :root_javascript
+          rule /(\n|\r|\r\n)/, String
+          rule /./, String
+      end
+
+      state:js_string1__1 do
+          rule /(\n|\r|\r\n)/, String
+          rule /./, String
+      end
+
+      state:js_string2__1 do
+          rule /(\n|\r|\r\n)/, String
+          rule /./, String
+      end
+
+      state:js_string3__1 do
           rule /(\n|\r|\r\n)/, String
           rule /./, String
       end
@@ -59,6 +58,60 @@ module Rouge
       state:json__3 do
           rule /((?:\\u[0-9a-f]{4})|(?:\\["\\\/bfnrt]))/, String::Escape
           rule /([^\"\\\n\r]+)/, String::Char
+          rule /(\n|\r|\r\n)/, String
+          rule /./, String
+      end
+
+      state:multi_line_comment__1 do
+          rule /(\n|\r|\r\n)/, String
+          rule /./, Comment
+      end
+
+      state:root_javascript do
+          rule /(^---$)/, Punctuation
+          rule /(\/\*)/, Comment, :multi_line_comment__1
+          rule /(\/\/.*$)/, Comment
+          rule /(\`)/, Punctuation, :js_string3__1
+          rule /(")/, Punctuation, :js_string1__1
+          rule /(')/, Punctuation, :js_string2__1
+          rule /([.,\\/#!$%\^&\*;:{}=\-_\[\]`~()])/, Punctuation
+          rule /(?:\b)(\.[_$a-zA-Z\xa0-\uffff][_$a-zA-Z0-9\xa0-\uffff]*)(\()/ do
+            groups Name::Function, Punctuation
+          end
+          rule /(?:\b)(?:do|if|in|for|let|new|try|var|case|else|enum|eval|false|null|this|true|void|with|break|catch|class|const|super|throw|while|yield|delete|export|import|public|return|static|switch|typeof|default|extends|finally|package|private|continue|debugger|function|arguments|interface|protected|implements|instanceof)(?:\b)/, Keyword::Declaration
+          rule /(?:\b)(var|let|const)(?:\b)/, Keyword::Namespace
+          rule /(?:\b)(return|yield)(?:\b)/, Keyword::Reserved
+          rule /(?:\b)([_$a-zA-Z\xa0-\uffff][_$a-zA-Z0-9\xa0-\uffff]*)(?:\b)/, Name::Variable
+          rule /(\+|-|\*|\\/|=|>|<|>=|<=|&|\||%|!|\^|\(|\))/, Punctuation
+          rule /(\b\d+)/, Number
+          rule /(\n|\r|\r\n)/, String
+          rule /./, String
+      end
+
+      state:root_lama do
+          rule /(^---$)/, Punctuation
+          rule /(#.*)/, Comment
+          rule /(GET|POST|PUT|DELETE|get|post|put|delete)/, Keyword
+          rule /(MULTIPART|multipart)/, Keyword
+          rule /(https?|\$\{.*\})([^\s\n\r]+)(?:\b)/ do
+            groups Name::Variable, Punctuation
+          end
+          rule /(\$\{.*\})(.*)(?:\b)/, Punctuation
+          rule /(\{)/, Punctuation, :json__1
+          rule /(\[)/, Punctuation, :json__2
+          rule /(,|\:)/, Punctuation
+          rule /(\")/, Punctuation, :json__3
+          rule /((?:\-?(?:0|(?:[1-9][0-9]*)))(?:(?:\.[0-9]*)(?:(?:e|E)(?:\+|\-)[0-9]+)?)?)/, Keyword::Type
+          rule /(true|false|null)/, Keyword::Type
+          rule /(.*)(:)([^#\n\r]+)/ do
+            groups Name::Constant, Punctuation, Literal
+          end
+          rule /(.*)(=)([^#\n\r]+)/ do
+            groups Name::Constant, Punctuation, Literal
+          end
+          rule /(.*)(@)([^#\n\r]+)/ do
+            groups Name::Constant, Punctuation, Literal
+          end
           rule /(\n|\r|\r\n)/, String
           rule /./, String
       end
