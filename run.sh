4b88c0b02ee9bece7a6a685758a927ffdecd3a2f
lovestaco <athreyac4@gmail.com>
2023-09-04 12:39:33 +0530

    Removing l2 --env option, coderefactor

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 clear
 make buildme
 chmod +x ./build/l2
-rm -rf /home/lovestaco/repos/Lama2Code/src/l2
-cp ./build/l2 /home/lovestaco/repos/Lama2Code/src/
+rm -rf /home/lovestaco/repos/Lama2Code/l2
+cp ./build/l2 /home/lovestaco/repos/Lama2Code/
