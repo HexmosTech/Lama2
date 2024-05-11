8d25a15d8f8507abbf54d1d93f0441075823bb06
Sreedeep CV <sreedeep200@gmail.com>
2024-05-11 20:09:03 +0530

    removing some useles code

--- buildAndPublish.sh
+++
@@ -1,7 +0,0 @@
-
-## Build and publish to master 
-GOOS=js GOARCH=wasm go build -a -gcflags=all="-l -B -wb=false" -ldflags="-w -s" -o static/main.wasm
-# cp /home/sreedeep/Downloads/Lama2/static/main.wasm /home/sreedeep/js-widget/dist/main.wasm
-scp /home/sreedeep/workspace/Lama2/static/main.wasm  ubuntu@master:/home/ubuntu/main.wasm
-ssh ubuntu@master "sudo mv main.wasm /var/www/lamawasm/"
-ssh ubuntu@master "sudo systemctl reload nginx"
