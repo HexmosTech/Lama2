782c193767de4dc86491a3c901b7357cf53552cc
RijulTP <rijulrajtkeey2@gmail.com>
2024-08-06 18:56:24 +0530

    Fixing deadlock issue

--- preprocess/webworker.go
+++ preprocess/webworker.go
@@ -3,14 +3,15 @@ package preprocess
 import (
 	"fmt"
 	"syscall/js"
-    "time"
-	"github.com/rs/zerolog/log"
 
-	"encoding/json"
+	// "sync"
+
+	"github.com/rs/zerolog/log"
 )
 
 var worker js.Value
-var responseChannel = make(chan js.Value)
+// var responseChannel = make(chan js.Value)
+// var responseChannel = make(chan js.Value, 1)
 // func initWebWorker() {
 // 	worker = js.Global().Get("Worker").New("worker.js")
 
@@ -26,22 +27,7 @@ func InitWebWorker() js.Value {
 	if worker.IsUndefined() {
         log.Debug().Msg("WW Initializing Web worker")
 		worker = js.Global().Get("Worker").New("worker.js")
-		worker.Call("addEventListener", "message", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-            //
-            data := args[0].Get("data")
-            dataJSON, err := json.Marshal(data)
-            if err != nil {
-                log.Error().Err(err).Msg("Failed to marshal data")
-            } else {
-                log.Debug().Msgf("WW: args[0].Get(\"data\"): %s", dataJSON)
-            }
 
-            //
-            responseChannel <- args[0].Get("data")
-         
-            log.Debug().Msg("WW: Returning Result from web worker:");
-			return nil
-		}))
 	}
 	return worker
 }
@@ -57,23 +43,114 @@ func RunCodeInWorker(chainCode string) {
     })
 }
 
+// Bugged version
+
+// func GetFromWorker(variableName string) string {
+//     // Ensure the worker is initialized
+//     log.Debug().Msg("WW: Check webworker")
+//     InitWebWorker()
+//     // Send the message to the worker
+//     worker.Call("postMessage", map[string]interface{}{
+//         "type":    "get",
+//         "payload": variableName,
+//     })
+
+//     log.Debug().Msg("WW: Webworker call complete")
+
+
+//     // Wait for the response from the worker
+//     result := <-responseChannel
+//     fmt.Println("WW: getfromworker webworker.go",variableName,result.Get("value").String())
+//     log.Debug().Msg("WW: Returning back the data")
+//     return result.Get("value").String()
+// }
+
+// Fix 1 
+
+// func GetFromWorker(variableName string) string {
+//     worker = InitWebWorker()
+//     var responseChannel = make(chan js.Value) // Ensure this is set to a new channel or appropriately reset
+//     worker.Call("postMessage", map[string]interface{}{
+//         "type":    "get",
+//         "payload": variableName,
+//     })
+
+//     worker.Call("addEventListener", "message", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+//         responseChannel <- args[0].Get("data")
+     
+//         log.Debug().Msg("WW: Returning Result from web worker:");
+//         return nil
+//     }))
+
+//     fmt.Println("WW: Webworker call complete:",variableName)
+//     var result js.Value
+//     result = <-responseChannel // This will block until a message is received
+
+//     fmt.Println("WW: getfromworker webworker.go", variableName, result.Get("value").String())
+//     log.Debug().Msg("WW: Returning back the data")
+//     return result.Get("value").String()
+// }
+
+// Fix 2: Deadlock fix
+
+// func GetFromWorker(variableName string) string {
+
+//     worker := InitWebWorker()
+//     responseChannel := make(chan js.Value,20)
+
+//     worker.Call("postMessage", map[string]interface{}{
+//         "type":    "get",
+//         "payload": variableName,
+//     })
+
+//     worker.Call("addEventListener", "message", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+//         select {
+//         case responseChannel <- args[0].Get("data"):
+//             log.Debug().Msg("WW: Returning Result from web worker:")
+//         default:
+//             log.Error().Msg("WW: responseChannel is full, cannot write data")
+//         }
+//         return nil
+//     }))
+//     fmt.Println("WW: Webworker call complete:", variableName)
+//     var result js.Value
+//     result = <-responseChannel
+//     fmt.Println("WW: getfromworker webworker.go", variableName, result.Get("value").String())
+//     return result.Get("value").String()
+// }
+
+// Fix 3: Deadlock fix + Wg.Done()
+
+
 func GetFromWorker(variableName string) string {
-    // Ensure the worker is initialized
-    log.Debug().Msg("WW: Check webworker")
-    InitWebWorker()
-    // Send the message to the worker
+    worker := InitWebWorker()
+    responseChannel := make(chan js.Value, 20) // Using a buffered channel
+
+    // var wg sync.WaitGroup
+    // wg.Add(1) 
+
     worker.Call("postMessage", map[string]interface{}{
         "type":    "get",
         "payload": variableName,
     })
 
-    log.Debug().Msg("WW: Webworker call complete")
+    worker.Call("addEventListener", "message", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+        // defer wg.Done() 
 
+        select {
+        case responseChannel <- args[0].Get("data"):
+            log.Debug().Msg("WW: Returning Result from web worker:")
+        default:
+            log.Error().Msg("WW: responseChannel is full, cannot write data")
+            }
+        return nil
+    }))
 
-    // Wait for the response from the worker
-    result := <-responseChannel
-    fmt.Println("WW: getfromworker webworker.go",variableName,result.Get("value").String())
-    log.Debug().Msg("WW: Returning back the data")
+    fmt.Println("WW: Webworker call complete:", variableName)
+    // wg.Wait() 
+
+    var result js.Value
+    result = <-responseChannel;
+    fmt.Println("WW: getfromworker webworker.go", variableName, result.Get("value").String())
     return result.Get("value").String()
 }
-
