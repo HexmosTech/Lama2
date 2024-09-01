4ebde26f0f30d0f64da60644168dc5ff401a8121
Sreedeep CV <sreedeep200@gmail.com>
2024-09-01 18:43:23 +0530

    fixing the mkdocs build

--- docs/Lama2/build.sh
+++ docs/Lama2/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 rm -rf site
 gomarkdoc --output docs/reference/parser.md ../../parser/
-gomarkdoc --output docs/reference/cmdexec.md ../../cmdexec/
+# Tags are not supported by gomarkdoc yet
+# gomarkdoc --output docs/reference/cmdexec.md ../../cmdexec/
 gomarkdoc --output docs/reference/cmdgen.md ../../cmdgen/
 gomarkdoc --output docs/reference/controller.md ../../controller/
 gomarkdoc --output docs/reference/lama2cmd.md ../../lama2cmd/
