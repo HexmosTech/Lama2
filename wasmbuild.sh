b84f2b443822202e4eb1147ed27c8f665f8d8dfe
lovestaco <athreyac4@gmail.com>
2025-02-24 21:35:12 +0530

    build script

--- wasmbuild.sh
+++ wasmbuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 GOOS=js GOARCH=wasm go build -a -gcflags=all="-l -B -wb=false" -ldflags="-w -s" -o static/main.wasm
-cp main.wasm /home/i3nux-mint/repos/liveapi/src/views/dist
+cp static/main.wasm /home/i3nux-mint/repos/liveapi/src/views/dist
 
 
