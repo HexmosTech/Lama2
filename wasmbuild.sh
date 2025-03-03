f36b2f780a68cd4b52de54fce45feb6deb51ec49
lovestaco <athreyac4@gmail.com>
2025-03-03 21:42:23 +0530

    build

--- wasmbuild.sh
+++ wasmbuild.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-export AWS_ACCESS_KEY_ID=""
-export AWS_SECRET_ACCESS_KEY=""
 GOOS=js GOARCH=wasm go build -a -gcflags=all="-l -B -wb=false" -ldflags="-w -s" -o static/main.wasm
-# cp static/main.wasm /home/i3nux-mint/repos/liveapi/src/views
-aws s3 cp static/main.wasm s3://temp-2d/temp/main.wasm --region ap-south-1
+cp static/main.wasm /home/lovestaco/hex/liveapi/src/mainmy.wasm
 
 
+ 
